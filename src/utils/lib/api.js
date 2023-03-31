cfb33926a948c4a9abb63a5a446b72d25b44a940
Baekys97 <117141532+Baekys97@users.noreply.github.com>
2023-03-31 18:20:24 +0900

    fix: proxy error fix (#3)

--- src/utils/lib/api.js
+++ src/utils/lib/api.js
@@ -3,3 +3,8 @@ import { client } from ".";
 export const getWeatherMidForecast = (key, token) => {
   return client(token).get(key);
 };
+
+
+export const getWeatherShortMain = (key, token) => {
+  return client(token).get(key);
+};
