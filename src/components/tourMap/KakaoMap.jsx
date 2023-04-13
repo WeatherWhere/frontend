a2ee831f528d5daa7d2c20347169bb338d98fa78
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-04-13 17:53:06 +0900

    (feat: marker title) 마커 장소 띄우기

--- src/components/tourMap/KakaoMap.jsx
+++ src/components/tourMap/KakaoMap.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { KAKAO_MAP_DATA } from "../../utils/const/position";
+import logotest from "../../styles/img/logotest.png";
 
 const { kakao } = window;
 
 export default function KakaoMap() {
   const [tourPositions, setTourPositions] = useState([]);
+
+  const markerImgSrc = logotest;
+  const markerImgSize = new kakao.maps.Size(64, 69);
+  const markerOption = { offset: new kakao.maps.Point(27, 69) };
+
+  const markerImg = new kakao.maps.MarkerImage(
+    markerImgSrc,
+    markerImgSize,
+    markerOption
+  );
 
   useEffect(() => {
     fetch("/mock/tour_data.json")
@@ -16,7 +27,6 @@ export default function KakaoMap() {
       });
   }, []);
 
-  console.log(tourPositions);
   useEffect(() => {
     const map = new kakao.maps.Map(document.getElementById("map"), {
       center: new kakao.maps.LatLng(
@@ -30,16 +40,52 @@ export default function KakaoMap() {
       map: map,
       averageCenter: true,
       minLevel: 10,
+      // disableClickZoom: true,
     });
 
     const markers = tourPositions.map((tourPosition, idx) => {
-      return new kakao.maps.Marker({
+      const marker = new kakao.maps.Marker({
         position: new kakao.maps.LatLng(tourPosition.mapy, tourPosition.mapx),
+        image: markerImg,
       });
+
+      const closeOverlay = () => {
+        overlay.setMap(null);
+      };
+
+      const content = `<div className="wrap">
+          <div className="info">
+            <div className="title">
+              ${tourPosition.title}
+              <div
+                class="close"
+                onClick={handleCloseOverlay}
+                title="닫기"
+              ></div>
+            </div>
+            <div className="body">
+              <div className="img">
+                <img src=${logotest} alt="logo" width=${73} height=${70} />
+              </div>
+            </div>
+          </div>
+        </div>`;
+
+      const overlay = new kakao.maps.CustomOverlay({
+        content: content,
+        position: marker.getPosition(),
+      });
+
+      kakao.maps.event.addListener(marker, "click", function () {
+        overlay.setMap(map);
+      });
+
+      return marker;
     });
 
     clusterer.addMarkers(markers);
   }, [tourPositions]);
+
   return <StMapWrapper id="map">hi</StMapWrapper>;
 }
 
