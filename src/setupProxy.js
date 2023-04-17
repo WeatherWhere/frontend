2751590b8068eb872cc939802b6b3b8150355aab
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-04-17 20:03:39 +0900

    (test: proxy 요청 시 secure option true)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    ["/weather","/air"],
+    ["/weather", "/air"],
     createProxyMiddleware({
-      target: "http://k8s-weatherw-weatherw-96e049a27a-1334965090.ap-northeast-2.elb.amazonaws.com",
+      target:
+        "http://k8s-weatherw-weatherw-96e049a27a-1334965090.ap-northeast-2.elb.amazonaws.com",
       changeOrigin: true,
+      secure: true,
     })
   );
 };
