9327d8c0ada51710fc5fe9c7d609ba38904b37fe
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-04-18 02:36:49 +0900

    (feat:cors header)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,14 @@ module.exports = function (app) {
     createProxyMiddleware({
       target:
         "http://k8s-weatherw-weatherw-96e049a27a-1334965090.ap-northeast-2.elb.amazonaws.com",
-      // changeOrigin: true,
+      changeOrigin: true,
+      headers: {
+        Origin: "http://localhost:3000",
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+      },
     })
   );
 };
--- src/utils/lib/index.js
+++ src/utils/lib/index.js
@@ -7,23 +7,26 @@ export const client = (token) => {
   if (token) {
     headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      Origin: "http://localhost:3000",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
       Authorization: token,
     };
   } else {
     headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      Origin: "http://localhost:3000",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
     };
   }
 
   return axios.create({
     baseURL: "/",
     headers,
-    withCredentials: true,
   });
 };
