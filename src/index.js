fc3647d6175f844d09655e87102b4e4e4b2e8892
Gyuminn <87220517+Gyuminn@users.noreply.github.com>
2023-04-22 03:03:35 +0900

    (feat: pwa) 운영 환경 - yarn build - serve -s build -l 3000 - 으로 pwa 반영 (#30)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/fonts/fonts";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+serviceWorkerRegistration.register();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
