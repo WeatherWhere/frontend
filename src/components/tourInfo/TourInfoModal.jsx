190aa737eb5ad3f6b5a7f49a64e0b77a7b6ab1b2
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-04-21 22:46:00 +0900

    (fix: modalInfo) 모달 바깥 영역(헤더)를 클릭 시에 모달 닫기, 이벤트 전파를 막아 헤더의 아이콘을 클릭해도 모달만 닫히도록 구현

--- src/components/tourInfo/TourInfoModal.jsx
+++ src/components/tourInfo/TourInfoModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import WeatherShortMainAll from "../weather/weatherShortMainAll/WeatherShortMainAll";
 import styled from "styled-components";
 import TourCommon from "./TourCommon";
@@ -12,7 +12,6 @@ import AirSubBottom from "../airRealTime/AirSubBottom";
 
 export default function TourInfoModal(props) {
   const { isOpen, setModalOpen, modalInfo } = props;
-  console.log(modalInfo);
 
   const [location] = useState({
     latitude: modalInfo.latitude,
@@ -20,8 +19,33 @@ export default function TourInfoModal(props) {
   });
   const [weatherOrAir, setWeatherOrAir] = useState(true);
 
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    // 이벤트 핸들러 함수
+    const handler = (event) => {
+      console.log(event);
+
+      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        event.preventDefault();
+        setModalOpen(false);
+      }
+    };
+
+    // 이벤트 핸들러 등록
+    document.addEventListener("mousedown", handler, { passive: false });
+    document.addEventListener("touchstart", handler, { passive: false }); // 모바일 대응
+
+    return () => {
+      // 이벤트 핸들러 해제
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler); // 모바일 대응
+    };
+  });
+
   return (
-    <RootPage isOpen={isOpen}>
+    <RootPage ref={modalRef} isOpen={isOpen}>
       <GlobalStyle />
       <Background>
         <StyledTabs defaultActiveKey="common" className="mb-3" justify>
@@ -100,6 +124,6 @@ const StClose = styled(Icon).attrs((props) => ({
   icon: props.name,
 }))`
   position: absolute;
-  right: 7px;
-  top: 7px;
+  right: 10px;
+  top: 10px;
 `;
